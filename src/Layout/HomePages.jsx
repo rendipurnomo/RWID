74578f8e23e66d1ff0bd1d1a824ea0e042a7414f
Rendi Purnomo <107144802+rendipurnomo@users.noreply.github.com>
2023-10-02 07:17:47 +0700

    edit name FaQ to Faq

--- src/Layout/HomePages.jsx
+++ src/Layout/HomePages.jsx
@@ -1,7 +1,7 @@
 import Hero from '../components/Hero/Hero';
 import Feature from '../components/Features/Feature';
 import Preview from '../components/Preview/Preview';
-import FaQ from '../components/FAQ/FaQ';
+import Faq from '../components/FAQ/FaQ';
 
 const HomePages = () => {
   return (
@@ -9,7 +9,7 @@ const HomePages = () => {
       <Hero />
       <Feature />
       <Preview />
-      <FaQ />
+      <Faq />
     </>
   );
 };
